cc92d4f4c2def0c65e37a41cceab1cc248b7769f
Sam Johnston <iam@samjohnston.co.uk>
2020-01-24 10:46:01 +0000

    Use babel ES6+ and setup nodemon, linting, husky, tests etc (#1)
    * setup prettier eslint babel nodemon
    
    * ignore dist folder setup eslint and husky

--- src/app/createImage.js
+++ src/app/createImage.js
@@ -1,25 +1,25 @@
-const Canvas = require("canvas");
+import Canvas from 'canvas';
 
-module.exports = async (req, res) => {
+const createImage = async (req, res) => {
   const {
-    imageSrc = "example.jpg",
+    imageSrc = 'example.jpg',
     width = 600,
     height = 400,
-    text = "example",
-    fontSize = "60px",
-    fontFamily = "sans-serif",
-    fontWeight = "normal",
-    fontColor = "#000000",
+    text = 'example',
+    fontSize = '60px',
+    fontFamily = 'sans-serif',
+    fontWeight = 'normal',
+    fontColor = '#000000',
     xPos = 0,
     yPos = 0
   } = req.query;
 
   const fontPath = `${process.cwd()}/src/fonts/IndieFlower-Regular.ttf`;
-  Canvas.registerFont(fontPath, { family: "Indie Flower" });
+  Canvas.registerFont(fontPath, { family: 'Indie Flower' });
 
   const backgroundPath = `${process.cwd()}/src/images/${imageSrc}`;
   const canvas = Canvas.createCanvas(width, height);
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext('2d');
 
   // Configure background
   const background = await Canvas.loadImage(backgroundPath);
@@ -27,19 +27,21 @@ module.exports = async (req, res) => {
 
   // Configure text
   const hexCodeRegex = /^[0-9A-F]{6}$/i;
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
   ctx.font = `${fontWeight} ${fontSize} ${fontFamily}`;
   ctx.fillStyle = hexCodeRegex.test(fontColor) ? `#${fontColor}` : fontColor;
 
-  const offset = parseInt(fontSize.replace("px", "") / 2);
+  const offset = parseInt(fontSize.replace('px', '') / 2);
   ctx.fillText(text, xPos, yPos - offset);
 
-  const finalImage = canvas.toBuffer("image/png", {
+  const finalImage = canvas.toBuffer('image/png', {
     compressionLevel: 3,
     filters: canvas.PNG_FILTER_NONE
   });
 
-  res.contentType("image/png");
+  res.contentType('image/png');
   res.send(finalImage);
 };
+
+export default createImage;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
-const express = require("express");
-const createImage = require("./createImage");
-
-const { SERVER_PORT } = process.env;
-const port = parseInt(SERVER_PORT, 10) || 8080;
-
-const server = express();
-server.disable("x-powered-by");
-server.get("/", (req, res) => createImage(req, res));
-
-server.get("/defined.png", (req, res) => {
-  req.query = {
-    text: "defined image",
-    fontColor: "white",
-    fontWeight: "bold",
-    fontFamily: "Indie Flower",
-    xPos: "300",
-    yPos: "220"
-  };
-
-  return createImage(req, res);
+require('@babel/register')({
+  ignore: [/node_modules/],
+  cache: true
 });
 
-server.listen(port, err => {
-  if (err) throw err;
+const startServer = require('./server').default;
 
-  console.info(`🚀  Server ready at http://localhost:${port}`);
-});
+startServer();
